2d86029d47ee11a0ed747ea1ba6d903b29809fb2
ThiruvengadaDixith <thiruvengadadixith@gmail.com>
2021-01-19 11:56:51 +0530

    Meditation-App

---
+++ app/src/main/java/com/innocrux/meditation_app/Signin.java
@@ -0,0 +1,14 @@
+package com.innocrux.meditation_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Signin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
